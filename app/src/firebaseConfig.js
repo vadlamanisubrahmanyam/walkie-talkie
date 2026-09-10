840ea4c76f116370150af18f2ed6678adaa1b06a
Phani Kumar <38527441+phankkumar@users.noreply.github.com>
2026-09-10 19:15:38 +0530

    Update firebaseConfig.js
    udpated firebaseconfig

--- app/src/firebaseConfig.js
+++ app/src/firebaseConfig.js
@@ -7,12 +7,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+  apiKey: "AIzaSyC7YnH27cYfIu-09uwcVgxC_P4oqhUe7hg",
+  authDomain: "walkie-talkie-6f866.firebaseapp.com",
+  projectId: "walkie-talkie-6f866",
+  storageBucket: "walkie-talkie-6f866.firebasestorage.app",
+  messagingSenderId: "1007349613163",
+  appId: "1:1007349613163:web:a60f73928a39fa103f272e",
+  measurementId: "G-P8G8E6MCXT"
 };
 
 export const app = initializeApp(firebaseConfig);
